b8ad787650ec904e51d48929376aef0db27b5b0e
praveenvtmn <37312987+praveenvtmn@users.noreply.github.com>
2022-10-11 17:19:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Praveen!!! This is your first fargate app!"));
+app.get("/", (req, res) => res.send("Hello Praveen!!! This is version 2.0"));
 
 app.get("/health", (req, res) => {
   res.status(200);
