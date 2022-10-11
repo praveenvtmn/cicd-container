63c966f33e51371888eced369a14b23692614df7
praveenvtmn <37312987+praveenvtmn@users.noreply.github.com>
2022-10-11 17:38:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Praveen!!! This is version 2.0"));
+app.get("/", (req, res) => res.send("Hello Praveen!!! This is version 3.0"));
 
 app.get("/health", (req, res) => {
   res.status(200);
